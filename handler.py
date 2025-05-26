1913ebe342db4ec93e9f7c75f2e91a091072832b
COMTECHTEL\harsh.patel <harsh.patel@comtech.com>
2025-05-26 12:50:57 -0400

    Added PIL

--- handler.py
+++ handler.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 import boto3
 from io import BytesIO
@@ -7,23 +6,22 @@ import os
 import uuid
 import json
 
-s3 = boto3.client('s3')
+s3 = boto3.client("s3")
 # dynamodb = boto3.client('dynamodb')
-dynamodb = boto3.resource(
-    'dynamodb', region_name=str(os.environ['REGION_NAME']))
-size = int(os.environ['THUMBNAIL_SIZE'])
-dbtable = str(os.environ['DYNAMODB_TABLE'])
+dynamodb = boto3.resource("dynamodb", region_name=str(os.environ["REGION_NAME"]))
+size = int(os.environ["THUMBNAIL_SIZE"])
+dbtable = str(os.environ["DYNAMODB_TABLE"])
 
 
 def s3_thumbnail_generator(event, context):
     # parse event
     print("EVENT:::", event)
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
-    img_size = event['Records'][0]['s3']['object']['size']
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
+    img_size = event["Records"][0]["s3"]["object"]["size"]
 
     # only create a thumbnail on non thumbnail pictures
-    if (not key.endswith("_thumbnail.png")):
+    if not key.endswith("_thumbnail.png"):
 
         # get the image
         image = get_s3_image(bucket, key)
@@ -40,7 +38,7 @@ def s3_thumbnail_generator(event, context):
 
 def get_s3_image(bucket, key):
     response = s3.get_object(Bucket=bucket, Key=key)
-    imagecontent = response['Body'].read()
+    imagecontent = response["Body"].read()
 
     file = BytesIO(imagecontent)
     img = Image.open(file)
@@ -48,11 +46,11 @@ def get_s3_image(bucket, key):
 
 
 def image_to_thumbnail(image):
-    return ImageOps.fit(image, (size, size), Image.ANTIALIAS)
+    return ImageOps.fit(image, (size, size), Image.LANCZOS)
 
 
 def new_filename(key):
-    key_split = key.rsplit('.', 1)
+    key_split = key.rsplit(".", 1)
     return key_split[0] + "_thumbnail.png"
 
 
@@ -60,17 +58,18 @@ def s3_get_thumbnail_urls(event, context):
     # get all image urls from the db and show in a json format
     table = dynamodb.Table(dbtable)
     response = table.scan()
-    data = response['Items']
+    data = response["Items"]
     # paginate through the results in a loop
-    while 'LastEvaluatedKey' in response:
-        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        data.extend(response['Items'])
+    while "LastEvaluatedKey" in response:
+        response = table.scan(ExclusiveStartKey=response["LastEvaluatedKey"])
+        data.extend(response["Items"])
 
     return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'body': json.dumps(data)
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(data),
     }
+
 
 # ====== RD - Read and Delete Only======
 
@@ -78,69 +77,68 @@ def s3_get_thumbnail_urls(event, context):
 def s3_get_item(event, context):
 
     table = dynamodb.Table(dbtable)
-    response = table.get_item(Key={
-        'id': event['pathParameters']['id']
-    })
+    response = table.get_item(Key={"id": event["pathParameters"]["id"]})
 
-    item = response['Item']
+    item = response["Item"]
 
     return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'},
-        'body': json.dumps(item),
-        'isBase64Encoded': False,
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": json.dumps(item),
+        "isBase64Encoded": False,
     }
 
 
 def s3_delete_item(event, context):
-    item_id = event['pathParameters']['id']
+    item_id = event["pathParameters"]["id"]
 
     # Set the default error response
     response = {
         "statusCode": 500,
-        "body": f"An error occured while deleting post {item_id}"
+        "body": f"An error occured while deleting post {item_id}",
     }
     table = dynamodb.Table(dbtable)
-    response = table.delete_item(Key={
-        'id': item_id
-    })
-    all_good_response = {
-        "deleted": True,
-        "itemDeletedId": item_id
-    }
+    response = table.delete_item(Key={"id": item_id})
+    all_good_response = {"deleted": True, "itemDeletedId": item_id}
 
-   # If deletion is successful for post
-    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+    # If deletion is successful for post
+    if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
         response = {
             "statusCode": 200,
-            'headers': {'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': '*'},
-            'body': json.dumps(all_good_response),
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            "body": json.dumps(all_good_response),
         }
     return response
+
+
 # Exercize--> Implement Update :)
 # ====== END RD - (no Create), Read, (no Update), Delete ======
 
 
 def s3_save_thumbnail_url_to_dynamo(url_path, img_size):
-    toint = float(img_size*0.53)/1000
+    toint = float(img_size * 0.53) / 1000
     table = dynamodb.Table(dbtable)
     response = table.put_item(
         Item={
-            'id': str(uuid.uuid4()),
-            'url': str(url_path),
-            'approxReducedSize': str(toint) + str(' KB'),
-            'createdAt': str(datetime.now()),
-            'updatedAt': str(datetime.now())
+            "id": str(uuid.uuid4()),
+            "url": str(url_path),
+            "approxReducedSize": str(toint) + str(" KB"),
+            "createdAt": str(datetime.now()),
+            "updatedAt": str(datetime.now()),
         }
     )
 
-# get all image urls from the bucked and show in a json format
+    # get all image urls from the bucked and show in a json format
     return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'body': json.dumps(response)
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(response),
     }
 
 
@@ -150,19 +148,19 @@ def upload_to_s3(bucket, key, image, img_size):
 
     # You MUST specify the file type because there is no file name to discern
     # it from
-    image.save(out_thumbnail, 'PNG')
+    image.save(out_thumbnail, "PNG")
     out_thumbnail.seek(0)
 
     response = s3.put_object(
-        ACL='public-read',
+        # ACL="public-read",
         Body=out_thumbnail,
         Bucket=bucket,
-        ContentType='image/png',
-        Key=key
+        ContentType="image/png",
+        Key=key,
     )
     print(response)
 
-    url = '{}/{}/{}'.format(s3.meta.endpoint_url, bucket, key)
+    url = "{}/{}/{}".format(s3.meta.endpoint_url, bucket, key)
 
     # save image url to db:
     s3_save_thumbnail_url_to_dynamo(url_path=url, img_size=img_size)
